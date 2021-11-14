1ead7404617ae404f6ade3a1f7e106bb434ccaa9
Michaela Šatarová <michaela.satarova.ml@gmail.com>
2021-11-14 14:41:16 +0100

    recipe book

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -6,7 +6,7 @@ export class Recipe{
    constructor(name: string, description: string, imagePath:string ){
        this.name = name;
        this.description = description;
-       this.imagePath = imagePath
+       this.imagePath = imagePath;
 
    }
 }
